cdd399a3cfa9e3451f1b76a8e2c2645fd9af33a2
hernan3154 <herny3154@gmail.com>
2026-03-29 16:22:37 -0300

    Fix Python version to 3.11.9 for Render compatibility

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True  # Modificado para encontrar el error en Render
-# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["apiaromazenpostgres.onrender.com", "127.0.0.1", "localhost"]
 
